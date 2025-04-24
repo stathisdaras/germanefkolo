b27a97aaff6f02090e31a99817442edd77b1576f
stathisdaras <efstathiosdaras@gmail.com>
2025-04-24 20:48:49 +0200

    finalized design, added tailwind

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'germanefkolo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('germanefkolo');
+    expect(app.translate('hero.title')).toEqual('germanefkolo');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TranslationService } from './services/translation.service';
+import { LanguageSwitcherComponent } from './components/language-switcher/language-switcher.component';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { SideNavigationComponent } from './components/side-navigation/side-navigation.component';
+
+interface Testimonial {
+  name: string;
+  comment: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    LanguageSwitcherComponent,
+    CarouselModule,
+    ButtonModule,
+    SideNavigationComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'germanefkolo';
+  responsiveOptions = [
+    {
+      breakpoint: '1024px',
+      numVisible: 3,
+      numScroll: 3
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 2,
+      numScroll: 2
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ];
+
+  constructor(private translationService: TranslationService) {}
+
+  translate(key: string): string {
+    return this.translationService.getTranslation(key);
+  }
+
+  getTestimonials(): Testimonial[] {
+    return this.translationService.getTestimonials();
+  }
+
+  scrollToAbout() {
+    const aboutSection = document.getElementById('about');
+    if (aboutSection) {
+      aboutSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
