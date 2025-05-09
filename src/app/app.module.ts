bbc6b0f0214442aec1ee1d41cb847b37bce4c789
stathisdaras <efstathiosdaras@gmail.com>
2025-05-09 20:03:25 +0200

    Refactor app.module.ts to transition from NgModule structure to bootstrapApplication for a simplified application initialization. Remove unnecessary imports and declarations, streamlining the module setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,6 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { LanguagePickerComponent } from './components/language-picker/language-picker.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LanguagePickerComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { } 
+bootstrapApplication(AppComponent, {
+  providers: []
+}).catch(err => console.error(err)); 
